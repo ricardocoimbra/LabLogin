43cab39cd765e6d254bc3a4961faa0885a6e72a5
Ricardo Coimbra - Eldorado <coimbra.rico@gmail.com>
2023-04-14 20:32:06 -0400

    Registro no Firebase

--- app/src/main/java/com/coimbra/lablogin/Register.java
+++ app/src/main/java/com/coimbra/lablogin/Register.java
@@ -1,16 +1,24 @@
 package com.coimbra.lablogin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
 
@@ -18,15 +26,21 @@ public class Register extends AppCompatActivity {
     MaterialButton btnReg;
     TextView gotoLogin;
 
+    ProgressBar progressBar;
+
+    FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_register);
 
         txtEmail = findViewById(R.id.editTextEmail);
         txtPassword = findViewById(R.id.editTextPassword);
         btnReg = findViewById(R.id.buttonRegister);
         gotoLogin = findViewById(R.id.loginNow);
+        progressBar = findViewById(R.id.progressbar);
 
         btnReg.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,7 +58,42 @@ public class Register extends AppCompatActivity {
                     Toast.makeText(Register.this, "O campo senha não pode estar vazio", Toast.LENGTH_SHORT).show();
                     return;
                 }
+
+                progressBar.setVisibility(View.VISIBLE);
+                btnReg.setVisibility(View.GONE);
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d("Success", "createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    progressBar.setVisibility(View.GONE);
+                                    btnReg.setVisibility(View.VISIBLE);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w("Failure", "createUserWithEmail:failure", task.getException());
+                                    progressBar.setVisibility(View.GONE);
+                                    Toast.makeText(Register.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
+
+        gotoLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
     }
 }
